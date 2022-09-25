68637673c8618d91e4ad98010300393d8f154d10
Héctor Vera <juniomilnovecientos@gmail.com>
2022-09-25 18:16:14 -0400

    Add option to fill array with an initial value

--- src/common/ContainerFactory.js
+++ src/common/ContainerFactory.js
@@ -1,9 +1,14 @@
-const createContainer = ({ capacity = 0, bitsPerSlot = 8 }) => {
-  const opts = { length: capacity };
-  if (bitsPerSlot <= 8 ) return new Uint8Array(opts);
-  if (bitsPerSlot <= 16) return new Uint16Array(opts);
-  if (bitsPerSlot <= 32) return new Uint32Array(opts);
-  return Array.from(opts);
+const DynamicArray = function ({ capacity }) {
+  return Array.from({ length: capacity });
+};
+
+const createContainer = ({ capacity = 0, bitsPerSlot = 8, initialValue = 0 }) => {
+  const options = { length: capacity };
+  const containerFromType = (type = Array) => new type(options).fill(initialValue);
+  if (bitsPerSlot <= 8 ) return containerFromType(Uint8Array);
+  if (bitsPerSlot <= 16) return containerFromType(Uint16Array);
+  if (bitsPerSlot <= 32) return containerFromType(Uint32Array);
+  return containerFromType(DynamicArray);
 }
 
 export const ContainerFactory = {
