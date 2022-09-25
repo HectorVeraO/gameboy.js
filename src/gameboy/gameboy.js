f52e20e25528f74f15fe033b3a1d9b9373d4717a
Héctor Vera <juniomilnovecientos@gmail.com>
2022-09-25 02:33:33 -0400

    Add end user interface

---
+++ src/gameboy/gameboy.js
@@ -0,0 +1,30 @@
+
+/**
+ * Abstraction representing the exposed functionality to the end user
+ */
+class Gameboy {
+  /** Cartridge's name */
+  game() {
+    // TODO: Implement
+  }
+
+  /** Load cartridge */
+  load() {
+    // TODO: Implement
+  }
+
+  /** Remove cartridge */
+  unload() {
+    // TODO: Implement
+  }
+
+  /** Turn on console */
+  power() {
+    // TODO: Implement
+  }
+
+  /** Play cartridge TODO: confirm */
+  run() {
+    // TODO: Implement
+  }
+}
