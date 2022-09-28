3d886ece2fe46a4acea9973eeff1475693c5c24b
Héctor Vera <juniomilnovecientos@gmail.com>
2022-09-28 01:40:32 -0400

    Add abstraction to handle banked memory

---
+++ src/common/MemoryFactory.js
@@ -0,0 +1,42 @@
+import { byte } from "./constants/InformationUnits";
+
+export class Memory {
+  constructor(bitCapacity, bankCount = 0) {
+    this.#byteCapacity = bitCapacity / byte;
+    this.#buffer = new ArrayBuffer(this.#byteCapacity);
+
+    this.#bankCount = bankCount || 1;
+    this.#bankByteCapacity = this.#byteCapacity / bankCount;
+    this.#banks = Array
+      .from({ length: bankCount })
+      .map((_, bankIndex) => {
+        const viewOffset = bankIndex * this.#bankByteCapacity;
+        const viewLength = this.#bankByteCapacity;
+        return new DataView(this.#buffer, viewOffset, viewLength);
+      });
+  }
+
+  read(address) {
+    return this.#getActiveBank().getUint8(address);
+  }
+
+  write(address, value) {
+    this.#getActiveBank().setUint8(address, value);
+  }
+
+  switchBank(bankIndex) {
+    this.activeBankIndex = bankIndex % this.#bankCount;
+  }
+
+  #buffer;
+  #byteCapacity;
+
+  #banks;
+  activeBankIndex;
+  #bankCount;
+  #bankByteCapacity;
+
+  #getActiveBank() {
+    return this.#banks[this.activeBankIndex];
+  }
+}
