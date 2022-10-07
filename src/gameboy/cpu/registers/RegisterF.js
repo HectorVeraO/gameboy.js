8c338743082729dc013086a12d3d2679a8b795c2
Héctor Vera <juniomilnovecientos@gmail.com>
2022-10-07 01:37:41 -0400

    Add flag setters

--- src/gameboy/cpu/registers/RegisterF.js
+++ src/gameboy/cpu/registers/RegisterF.js
@@ -8,26 +8,53 @@ export class RegisterF extends Uint8 {
 
   /** Zero flag */
   get Z() {
-    return this.bit(7);
+    return this.bit(RegisterF.#position.z);
   }
 
   /** Subtraction flag (BCD) */
   get N() {
-    return this.bit(6);
+    return this.bit(RegisterF.#position.n);
   }
 
   /** Half carry flag (BCD) */
   get H() {
-    return this.bit(5);
+    return this.bit(RegisterF.#position.h);
   }
 
   /** Carry flag */
   get C() {
-    return this.bit(4);
+    return this.bit(RegisterF.#position.c);
   }
 
   /** Carry flag, alias of C */
   get Cy() {
     return this.C;
   }
+  
+  set Z(value = 1) {
+    this.setBit(RegisterF.#position.z, value);
+  }
+
+  set N(value = 1) {
+    this.setBit(RegisterF.#position.n, value);
+  }
+
+  set H(value = 1) {
+    this.setBit(RegisterF.#position.h, value);
+  }
+
+  set C(value = 1) {
+    this.setBit(RegisterF.#position.c, value);
+  }
+
+  set Cy(value = 1) {
+    this.setBit(RegisterF.#position.c, value);
+  }
+
+  static #position = {
+    z: 7,
+    n: 6,
+    h: 5,
+    c: 4,
+  };
 }
