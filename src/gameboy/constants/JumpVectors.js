869e5899117eef71f2f4cda4077210469f895a5a
Héctor Vera <juniomilnovecientos@gmail.com>
2022-10-06 22:19:52 -0400

    Add jump vector constants

---
+++ src/gameboy/constants/JumpVectors.js
@@ -0,0 +1,5 @@
+/** RST Instruction symbol, just in case I need to differentiate */
+export const RST_INSTRUCTION = Symbol('RST instruction');
+
+/** Interrupt symbol, just in case I need to differentiate */
+export const INTERRUPT = Symbol('Interrupt');
