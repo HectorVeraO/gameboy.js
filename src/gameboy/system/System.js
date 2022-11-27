7709607b14ba8eb44f27527cd64697db86baadb1
Héctor Vera <juniomilnovecientos@gmail.com>
2022-11-27 19:23:55 -0400

    Wire CPU to the System

--- src/gameboy/system/System.js
+++ src/gameboy/system/System.js
@@ -1,5 +1,7 @@
 import { ContainerFactory } from "@common/ContainerFactory";
 import { byte, KiB } from "@common/constants/InformationUnits";
+import { SharpLR35902 } from "@gameboy/cpu/Cpu";
+import { Cartridge } from "@gameboy/cartridge/Cartridge";
 
 /**
  * GameBoy SoC representation (plus the Work RAM and Video RAM for convenience)
@@ -9,7 +11,13 @@ import { byte, KiB } from "@common/constants/InformationUnits";
 export class System {
   static bitsPerWord = 8;
 
-  cartridge = null;
+  cartridge = new Cartridge(new Uint8Array(0)); // FIXME: Handle as "Empty Cartridge"
+
+  cpu;
+
+  ppu;
+
+  apu;
 
   /** Video RAM */
   vram = System.#createMemory(16 * KiB);
@@ -36,6 +44,87 @@ export class System {
   ier = System.#createMemory(0x0001 * byte);
   
   constructor() {
+    this.cpu = new SharpLR35902(this.#readMemory, this.#writeMemory);
+  }
+
+  #readMemory(address) {
+    const boundedAddress = address & 0xFFFF;
+
+    if (boundedAddress < 0x4000)
+      return this.cartridge.read(address);
+    
+    if (boundedAddress < 0x8000)
+      return this.cartridge.read(address);
+    
+    if (boundedAddress < 0xA000)
+      return this.cartridge.read(address);
+    
+    if (boundedAddress < 0xC000)
+      return this.cartridge.read(address);
+
+    if (boundedAddress < 0xD000)
+      return this.cartridge.read(address);
+
+    if (boundedAddress < 0xE000)
+      return this.cartridge.read(address);
+
+    if (boundedAddress < 0xFE00)
+      return this.cartridge.read(boundedAddress - (0xE000 - 0xC000)); // Mirror from 0xC000 to 0xDDFF
+
+    if (boundedAddress < 0xFEA0)
+      return this.oam[boundedAddress - 0xFE00];
+
+    if (boundedAddress < 0xFF00)
+      return this.fmem[boundedAddress - 0xFEA0]; // Nintendo says use of this area is prohibited
+
+    if (boundedAddress < 0xFF80)
+      return this.ior[boundedAddress - 0xFF00];
+
+    if (boundedAddress < 0xFFFF)
+      return this.hram[boundedAddress - 0xFF80];
+
+    if (boundedAddress === 0xFFFF)
+      return this.ier[0];
+  }
+
+  #writeMemory(address, byte) {
+    const boundedAddress = address & 0xFFFF;
+
+    if (boundedAddress < 0x4000)
+      this.cartridge.write(address, byte);
+    
+    else if (boundedAddress < 0x8000)
+      this.cartridge.write(address, byte);
+    
+    else if (boundedAddress < 0xA000)
+      this.cartridge.write(address, byte);
+    
+    else if (boundedAddress < 0xC000)
+      this.cartridge.write(address, byte);
+
+    else if (boundedAddress < 0xD000)
+      this.cartridge.write(address, byte);
+
+    else if (boundedAddress < 0xE000)
+      this.cartridge.write(address, byte);
+
+    else if (boundedAddress < 0xFE00)
+      this.cartridge.write(boundedAddress - (0xE000 - 0xC000), byte); // Mirror from 0xC000 to 0xDDF = byteF
+
+    else if (boundedAddress < 0xFEA0)
+      this.oam[boundedAddress - 0xFE00] = byte;
+
+    else if (boundedAddress < 0xFF00)
+      this.fmem[boundedAddress - 0xFEA0]; // Nintendo says use of this area is prohibite = byted
+
+    else if (boundedAddress < 0xFF80)
+      this.ior[boundedAddress - 0xFF00] = byte;
+
+    else if (boundedAddress < 0xFFFF)
+      this.hram[boundedAddress - 0xFF80] = byte;
+
+    else if (boundedAddress === 0xFFFF)
+      this.ier[0] = byte;
   }
 
   static #bitsPerWord = 8;
