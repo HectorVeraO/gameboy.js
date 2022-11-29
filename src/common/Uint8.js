2e5307c2ebc4716a12c46168efcd52fe6b56dab6
Héctor Vera <juniomilnovecientos@gmail.com>
2022-11-29 00:51:54 -0400

    Add bit range abstractions

--- src/common/Uint8.js
+++ src/common/Uint8.js
@@ -1,3 +1,5 @@
+import { MASK_BY_BIT_WIDTH } from "./Number";
+
 export class Uint8 {
   constructor(value = 0x00) {
     this.set(value);
@@ -18,6 +20,33 @@ export class Uint8 {
 
   clearBit(position) {
     this.#value &= ~(1 << position);
+  }
+  
+  /**
+   * Unchecked bit range for 8-bit integers
+   * @param {uint3} position 
+   * @param {uint3} size 
+   * @returns uint8
+   */
+  bitRange(position, size) {
+    let slice = this << (8 - position - size);
+    slice &= 0xFF;
+    slice >>>= (8 - size);
+    slice &= 0xFF;
+    return slice;
+  }
+
+  /**
+   * Unchecked bit range update for 8-bit integers
+   * @param {uint3} position 
+   * @param {uint3} size 
+   * @param {uint8} value 
+   */
+  setBitRange(position, size, value) {
+    const rangeMask = MASK_BY_BIT_WIDTH[size];
+    const boundedValue = value & rangeMask;
+    const byte = (this & ~(rangeMask << position)) | (boundedValue << position);
+    this.set(byte);
   }
 
   valueOf() {
