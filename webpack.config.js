948b4dabd0a4322c389ce62bc5b7809c2d5ad48c
Héctor Vera <juniomilnovecientos@gmail.com>
2022-12-11 15:17:29 -0400

    Setup dev server with hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/client/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   resolve: {
-    extensions: ['js', 'mjs'],
+    extensions: ['.js', '.mjs'],
     alias: {
       '@client': path.resolve(__dirname, 'src', 'client'),
       '@gameboy': path.resolve(__dirname, 'src', 'gameboy'),
       '@common': path.resolve(__dirname, 'src', 'common'),
-    }
-  }
+    },
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'GameBoy.js',
+      template: './src/client/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
 };
