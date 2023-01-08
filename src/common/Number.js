5eeed2f2a747faf8c8b5516a7a561fc7fb9506d7
Héctor Vera <juniomilnovecientos@gmail.com>
2023-01-08 17:53:25 -0400

    Parameterize hex prefix and pad with leading zeroes

--- src/common/Number.js
+++ src/common/Number.js
@@ -2,4 +2,4 @@ export const MASK_BY_BIT_WIDTH = [ 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7
 
 export const getMinimumBitWidth = (number) => Math.floor(Math.log2(number)) + 1;
 
-export const hexStr = (number, prefix = '0x') => `${prefix}${Number(number).toString(16)}`;
+export const hexStr = (number, prefix = '0x', length = 8) => `${prefix}${Number(number).toString(16).padStart(length, '0').toUpperCase()}`;
