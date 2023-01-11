9efd468676eab33446882b74f0006dc8c8a993fc
Héctor Vera <juniomilnovecientos@gmail.com>
2023-01-10 23:01:29 -0400

    Handle RW to serial data transfer registers

--- src/gameboy/io/LinkController.js
+++ src/gameboy/io/LinkController.js
@@ -1,3 +1,7 @@
+import { hexStr } from "@common/Number";
+import { RegisterSB } from "./registers/RegisterSB";
+import { RegisterSC } from "./registers/RegisterSC";
+
 /**
  * Serial Data Transfer (Link Cable) memory mapped register
  * https://gbdev.io/pandocs/Serial_Data_Transfer_(Link_Cable).html#serial-data-transfer-link-cable
@@ -14,6 +18,10 @@ export class LinkController {
    * @returns uint8 | null
    */
   guardRead = (address) => {
+    if (address === RegisterSB.ADDRESS)
+      return this.#SB.valueOf();
+    if (address === RegisterSC.ADDRESS)
+      return this.#SC.valueOf();
     return null;
   }
 
@@ -23,9 +31,41 @@ export class LinkController {
    * @returns boolean
    */
   guardWrite = (address, byte) => {
-    return false;
+    let writePerformed = true;
+    if (address === RegisterSB.ADDRESS)
+      this.#SB = byte;
+    else if (address === RegisterSC.ADDRESS)
+      this.#SC = byte;
+    else
+      writePerformed = false;
+    
+    if (writePerformed) {
+      console.log(`${hexStr(address, '$', 4)} = ${hexStr(byte, '$', 2)} ; ${String.fromCharCode(byte)}`);
+    }
+    return writePerformed;
   }
 
   #readMemory;
   #writeMemory;
+
+  #register = {
+    SB:new RegisterSB(),
+    SC:new RegisterSC(),
+  }
+
+  get #SB() {
+    return this.#register.SB;
+  }
+
+  get #SC() {
+    return this.#register.SC;
+  }
+
+  set #SB(byte) {
+    this.#register.SB.set(byte);
+  }
+
+  set #SC(byte) {
+    this.#register.SC.set(byte);
+  }
 }
