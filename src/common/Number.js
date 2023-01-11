99b38b54efdbf46253c60b749533c0aebb705abf
Héctor Vera <juniomilnovecientos@gmail.com>
2023-01-10 20:02:13 -0400

    Add function to display decimal strings

--- src/common/Number.js
+++ src/common/Number.js
@@ -3,3 +3,5 @@ export const MASK_BY_BIT_WIDTH = [ 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7
 export const getMinimumBitWidth = (number) => Math.floor(Math.log2(number)) + 1;
 
 export const hexStr = (number, prefix = '0x', length = 8) => `${prefix}${Number(number).toString(16).padStart(length, '0').toUpperCase()}`;
+
+export const decStr = (number, prefix = '', length = 8) => `${prefix}${Number(number).toString(10).padStart(length, '0').toUpperCase()}`;
